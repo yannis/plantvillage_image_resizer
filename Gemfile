25f4182a8af7766b36b83238cbb512a563f65a03
Yannis Jaquet <yannisjaquet@mac.com>
2015-10-02 16:42:51 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rubyzip', '>= 1.0.0'
+gem 'rmagick', '2.15.4', require: 'RMagick'
+gem 'byebug'
---
+++ resize.rb
@@ -0,0 +1,78 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'zip'
+require 'RMagick'
+
+def is_image?(filename)
+  !filename.downcase.match(/\.jpeg|\.jpg/).nil?
+end
+
+def two_step_resize(img, filename, max_x, max_y)
+  x = img.columns
+  y = img.rows
+
+  #make sure it's a float w/ the 1.0*
+  ratio = (1.0*x)/y
+
+  new_y = max_y
+  new_x = ratio * new_y
+
+  if (new_x > max_x)
+    new_x = max_x
+    new_y = new_x / ratio
+  end
+
+  # do the change in two steps, first the height
+  img.resize!(x, new_y);
+  #then the width
+  img.resize!(new_x, new_y)
+
+  #save it, with the least compression to get a better image
+  img.write(filename){self.quality=100}
+
+end
+
+#spin the image before resizing
+def resize_with_rotate(img, output_filename, max_x, max_y)
+  res = img.rotate(90).resize_to_fit(max_y, max_x).rotate(-90)
+  res.write(output_filename){self.quality=100}
+end
+
+def unzip_file (file)
+  destination = "."
+  Zip::File.open(file) do |zip_file|
+    zip_file.each do |f|
+      f_path = File.join(destination, f.name)
+      FileUtils.mkdir_p(File.dirname(f_path)) unless File.exist?(f_path)
+      f.extract(f_path)
+    end
+  end
+end
+
+# unzip zipped dirs
+zipped_dirs = Dir.entries('.').select {|entry| File.file?(File.join('.', entry)) and entry.match(/plantimages/) and entry.match(/\.zip$/) }
+zipped_dirs.each do |zipped_dir|
+  if unzip_file zipped_dir
+    FileUtils.rm(zipped_dir)
+  end
+  FileUtils.rmdir("./__MACOSX")
+end
+
+#list all plantimages dirs
+directories = Dir.entries('.').select {|entry| File.directory? File.join('.',entry) and !(entry =='.' || entry == '..')and entry.match(/plantimages/) }
+
+directories.each do |directory|
+  images = Dir.entries(directory).select do |filename|
+    is_image?(filename)
+  end
+  images.each do |image|
+    image_fullpath = File.join(directory, image)
+    p "image_fullpath: #{image_fullpath}"
+    img = Magick::Image.read(image_fullpath).first
+    new_image_filename = image_fullpath.gsub(image_basename, image_basename+"_resized")
+    two_step_resize(img, new_image_filename , 600, 800)
+  end
+end
+
+
+
